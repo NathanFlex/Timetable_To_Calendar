9550765e9b1ce7a56952daf46d07c7c62e271cbf
JoelJames03 <53299215+JoelJames03@users.noreply.github.com>
2021-12-10 10:49:51 +0530

    Add files via upload
    Added OCRapi.py, which converts the user inputted image to a csv file

---
+++ OCRapi.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import csv
+
+pathOfImage = 'timetable.png'
+
+url = 'https://app.nanonets.com/api/v2/ObjectDetection/Model/df672ace-ef4c-4bd5-b514-373bf377ee44/LabelFile/'
+
+data = {'file': open(pathOfImage, 'rb')}
+
+response = requests.post(url, auth  =requests.auth.HTTPBasicAuth('y2gh8eWyLi2Bq1W6UTbdsj0gJIZ0JZZf', ''), files = data)
+
+print(response.text)
+
+loads = json.loads(response.text)
+
+with open('Timetable.csv','w', newline='') as fcsv:
+    row = []
+    writer = csv.writer(fcsv)
+    for i in range(len(loads["result"][0]['prediction'][0]['cells'])):                #15 columns
+        row.append(loads["result"][0]['prediction'][0]['cells'][i]['text'])
+        if len(row) == 15:
+            writer.writerow(row)
+            row = []
+            counter=0
+
